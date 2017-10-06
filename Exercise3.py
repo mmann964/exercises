14e432befb04a2914bb7e165f1341f782e0b7743
Melissa Mann <melissa.mann@pluribusnetworks.com>
2017-10-06 09:54:24 -0700

    Adding exercise 3

---
+++ Exercise3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+num = int(raw_input("Number please:  "))
+
+for element in a:
+    if element < num:
+        b.append(element)
+
+for element in b:
+    print element
